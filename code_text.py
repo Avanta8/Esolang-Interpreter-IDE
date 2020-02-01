ac0f01c94f72221f44d5aaf29af558a9a77716ab
Harry Min <happyjet123@gmail.com>
2020-02-01 18:31:54 +0000

    highlight errors

--- code_text.py
+++ code_text.py
@@ -6,14 +6,15 @@ import lexers
 
 class CodeText(Qsci.QsciScintilla):
 
-    LEXERS = {
+    _LEXERS = {
         FileTypes.NONE: lexers.NoneLexer,
         FileTypes.BRAINFUCK: lexers.BrainfuckLexer,
         FileTypes.PYTHON: lexers.NoneLexer,
     }
 
-    # According to docs, The first 8 are normally used by lexers
-    _current_position_indicator_number = 20
+    # According to docs, The 0-7 are normally used by lexers
+    _CURRENT_POSITION_INDICATOR = 8
+    _ERROR_INDICATOR = 9
 
     def __init__(self, parent=None):
         super().__init__(parent=parent)
@@ -24,6 +25,7 @@ class CodeText(Qsci.QsciScintilla):
         self.setLexer(None)
 
         self._last_command_position = (0, 0)
+        self._error_locations = set()
 
     def init_settings(self):
         # self.setEolMode(Qsci.QsciScintilla.SC_EOL_LF)
@@ -51,19 +53,37 @@ class CodeText(Qsci.QsciScintilla):
         self.setIndentationGuidesBackgroundColor(QtGui.QColor(211, 211, 211))
         self.setIndentationGuidesForegroundColor(QtGui.QColor(211, 211, 211))
 
-        self.indicatorDefine(self.FullBoxIndicator, self._current_position_indicator_number)
-        self.setIndicatorDrawUnder(True, self._current_position_indicator_number)
-        self.setIndicatorForegroundColor(QtGui.QColor('grey'))
-        self.setIndicatorOutlineColor(QtGui.QColor('grey'))
+        self.indicatorDefine(self.FullBoxIndicator, self._CURRENT_POSITION_INDICATOR)
+        self.setIndicatorDrawUnder(True, self._CURRENT_POSITION_INDICATOR)
+        self.setIndicatorForegroundColor(QtGui.QColor('grey'), indicatorNumber=self._CURRENT_POSITION_INDICATOR)
+        self.setIndicatorOutlineColor(QtGui.QColor('grey'), indicatorNumber=self._CURRENT_POSITION_INDICATOR)
+
+        self.indicatorDefine(self.FullBoxIndicator, self._ERROR_INDICATOR)
+        self.setIndicatorDrawUnder(True, self._ERROR_INDICATOR)
+        self.setIndicatorForegroundColor(QtGui.QColor('red'), indicatorNumber=self._ERROR_INDICATOR)
+        self.setIndicatorOutlineColor(QtGui.QColor('red'), indicatorNumber=self._ERROR_INDICATOR)
 
     def set_filetype(self, filetype):
-        self.setLexer(self.LEXERS[filetype](self))
+        self.setLexer(self._LEXERS[filetype](self))
 
     def highlight_position(self, position, chars):
-        self.SendScintilla(self.SCI_SETINDICATORCURRENT, self._current_position_indicator_number)
+        self.SendScintilla(self.SCI_SETINDICATORCURRENT, self._CURRENT_POSITION_INDICATOR)
         self.SendScintilla(self.SCI_INDICATORCLEARRANGE, *self._last_command_position)
         self.SendScintilla(self.SCI_INDICATORFILLRANGE, position, chars)
         self._last_command_position = (position, chars)
+
+    def highlight_error(self, position, chars):
+        self.SendScintilla(self.SCI_SETINDICATORCURRENT, self._ERROR_INDICATOR)
+        self.SendScintilla(self.SCI_INDICATORFILLRANGE, position, chars)
+        self._error_locations.add((position, chars))
+
+    def remove_errors(self):
+        if not self._error_locations:
+            return
+        self.SendScintilla(self.SCI_SETINDICATORCURRENT, self._ERROR_INDICATOR)
+        for position, chars in self._error_locations:
+            self.SendScintilla(self.SCI_INDICATORCLEARRANGE, position, chars)
+        self._error_locations = set()
 
 
 if __name__ == "__main__":
