e6282270abc389b903fc839bfe1a664f1bff2698
Harry Min <harryhmin@gmail.com>
2021-09-24 13:48:13 +0100

    create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='esolang-IDE',
+    version='0.0.1',
+    author='Harry Min',
+    author_email='harryhmin@gmail.com',
+    description='An IDE for esoteric languages',
+    packages=find_packages(),
+    python_requires='>=3.7',
+)
