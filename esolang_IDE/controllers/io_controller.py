c144418bf5142b6fa681974709eae832d034b2da
Harry Min <harryhmin@gmail.com>
2021-09-25 16:49:34 +0100

    Split up responsibilities

--- esolang_IDE/controllers/io_controller.py
+++ esolang_IDE/controllers/io_controller.py
@@ -1,6 +1,7 @@
-from typing import NamedTuple
+import enum
+from typing import NamedTuple, Callable
 
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtWidgets
 
 from .page_controller import PageController
 
@@ -15,45 +16,48 @@ class _PageInfo(NamedTuple):
     fileinfo: FileInfo
 
 
-class IOController:
-    def __init__(self):
+class _FileOperation(enum.Enum):
+    new = enum.auto()
+    save = enum.auto()
+    open = enum.auto()
+    saveas = enum.auto()
 
-        self.notebook = Notebook()
-        self.notebook.page_closed.connect(self._page_closed)
 
-        self.pages: dict[EditorPage, _PageInfo] = {}
+class _RunOperation(enum.Enum):
+    run = enum.auto()
+    visualiser = enum.auto()
+
+
+class _OperationHandler:
+
+    _op2fn: dict[enum.Enum, Callable]
+
+    def __init__(self, notebook: Notebook, pages: dict[EditorPage, _PageInfo]):
+        self.notebook = notebook
+        self.pages = pages
+
+    def handle(self, operation: enum.Enum):
+        return self._op2fn[operation]()
+
+
+class _FileOperationHandler(_OperationHandler):
+    def __init__(self, notebook: Notebook, pages: dict[EditorPage, _PageInfo]):
+        self.notebook = notebook
+        self.pages = pages
+
+        self._op2fn = {
+            _FileOperation.new: self._new,
+            _FileOperation.save: self._save,
+            _FileOperation.open: self._open,
+            _FileOperation.saveas: self._saveas,
+        }
 
         self._file_dialog_filter = 'All Files (*);;Text Files (*.txt);;Brainfuck (*.b)'
 
-        self.init_mainwindow()
+    def _new(self):
+        self._create_new_page(FileInfo.from_empty())
 
-    def init_mainwindow(self):
-        self.ide = IDE(
-            menu_layout={
-                'File': (
-                    ('New', 'Ctrl+N', 'New file', self.file_new),
-                    ('Open', 'Ctrl+O', 'Open file', self.file_open),
-                    ('Save', 'Ctrl+S', 'Save file', self.file_save),
-                    ('Save As', 'Ctrl+Shift+S', 'Save as', self.file_saveas),
-                ),
-                'Run': (
-                    ('Run code', 'Ctrl+B', 'Run code', self.run_code),
-                    (
-                        'Open visualiser',
-                        'Ctrl+Shift+B',
-                        'Visualise execution',
-                        self.open_visualier,
-                    ),
-                ),
-            }
-        )
-        self.ide.setGeometry(300, 200, 1280, 720)
-        self.ide.setCentralWidget(self.notebook)
-
-    def file_new(self):
-        self.create_new_page(FileInfo.from_empty())
-
-    def file_open(self):
+    def _open(self):
         filepath, extension = QtWidgets.QFileDialog.getOpenFileName(
             filter=self._file_dialog_filter
         )
@@ -61,21 +65,21 @@ class IOController:
             return
 
         fileinfo = FileInfo.from_filepath(filepath)
-        self.create_new_page(fileinfo, fileinfo.read())
+        self._create_new_page(fileinfo, fileinfo.read())
 
-    def file_save(self):
+    def _save(self):
         current_page = self.notebook.current_page()
         if current_page is None:
             return
 
         page_controller, fileinfo = self.pages[current_page]
         if fileinfo.is_empty():
-            self.file_saveas()
+            self._saveas()
         else:
             fileinfo.write(page_controller.get_text())
             self._update_pagename(current_page)
 
-    def file_saveas(self):
+    def _saveas(self):
         current_page = self.notebook.current_page()
         if current_page is None:
             return
@@ -90,23 +94,9 @@ class IOController:
         page_controller, fileinfo = self.pages[current_page]
         fileinfo.update_from_filepath(filepath)
         page_controller.set_filetype(fileinfo.filetype)
-        self.file_save()
+        self._save()
 
-    def run_code(self):
-        page = self.notebook.current_page()
-        if page is None:
-            return
-
-        self.pages[page].controller.run_code()
-
-    def open_visualier(self):
-        page = self.notebook.current_page()
-        if page is None:
-            return
-
-        self.pages[page].controller.open_visualiser()
-
-    def create_new_page(self, fileinfo: FileInfo, text=''):
+    def _create_new_page(self, fileinfo: FileInfo, text=''):
         page = EditorPage()
         self.notebook.add_tab_to_current(page, fileinfo.filename or 'Untitled')
 
@@ -115,10 +105,6 @@ class IOController:
         page_controller.text_changed.connect(self._page_text_changed)
 
         self.pages[page] = _PageInfo(page_controller, fileinfo)
-
-    def _page_closed(self, page):
-        # Clean up reference
-        self.pages.pop(page)
 
     def _page_text_changed(self, page):
         fileinfo = self.pages[page].fileinfo
@@ -131,3 +117,76 @@ class IOController:
         self.notebook.set_page_tab_text(
             page, f'{"*. " if fileinfo.changed else ""}{fileinfo.filename or "Untitled"}'
         )
+
+
+class _RunOperationHandler(_OperationHandler):
+    def __init__(self, notebook: Notebook, pages: dict[EditorPage, _PageInfo]):
+        super().__init__(notebook, pages)
+
+        self._op2fn = {
+            _RunOperation.run: self._run_code,
+            _RunOperation.visualiser: self._open_visualier,
+        }
+
+    def _run_code(self):
+        page = self.notebook.current_page()
+        if page is None:
+            return
+
+        self.pages[page].controller.run_code()
+
+    def _open_visualier(self):
+        page = self.notebook.current_page()
+        if page is None:
+            return
+
+        self.pages[page].controller.open_visualiser()
+
+
+class IOController:
+    def __init__(self):
+
+        self.notebook = Notebook()
+        self.notebook.page_closed.connect(self._page_closed)
+
+        self.pages: dict[EditorPage, _PageInfo] = {}
+
+        self.file_handler = _FileOperationHandler(self.notebook, self.pages)
+        self.run_handler = _RunOperationHandler(self.notebook, self.pages)
+
+        self.init_mainwindow()
+
+    def init_mainwindow(self):
+        file_handle = lambda op: (lambda: self.file_handler.handle(op))
+        run_handle = lambda op: (lambda: self.run_handler.handle(op))
+
+        self.ide = IDE(
+            menu_layout={
+                'File': (
+                    ('New', 'Ctrl+N', 'New file', file_handle(_FileOperation.new)),
+                    ('Open', 'Ctrl+O', 'Open file', file_handle(_FileOperation.open)),
+                    ('Save', 'Ctrl+S', 'Save file', file_handle(_FileOperation.save)),
+                    (
+                        'Save As',
+                        'Ctrl+Shift+S',
+                        'Save as',
+                        file_handle(_FileOperation.saveas),
+                    ),
+                ),
+                'Run': (
+                    ('Run code', 'Ctrl+B', 'Run code', run_handle(_RunOperation.run)),
+                    (
+                        'Open visualiser',
+                        'Ctrl+Shift+B',
+                        'Visualise execution',
+                        run_handle(_RunOperation.visualiser),
+                    ),
+                ),
+            }
+        )
+        self.ide.setGeometry(300, 200, 1280, 720)
+        self.ide.setCentralWidget(self.notebook)
+
+    def _page_closed(self, page):
+        # Clean up reference
+        self.pages.pop(page)
