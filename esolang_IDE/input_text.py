4e531a00a53e92bda9bc0113477f9c92c37d4d2a
Harry Min <harryhmin@gmail.com>
2021-09-27 02:59:31 +0100

    HighlightInputText now can update in the main thread if it was called in a different thread

--- esolang_IDE/input_text.py
+++ esolang_IDE/input_text.py
@@ -23,23 +23,23 @@ class StandardInputText(QtWidgets.QPlainTextEdit):
         self._reset()
 
     def _reset(self):
-        self.prev_input_indexes = [0, 0]
+        self._prev_input_indexes = [0, 0]
 
     def next_(self):
         text = self.toPlainText()
-        last_input = self.prev_input_indexes[-1]
+        last_input = self._prev_input_indexes[-1]
 
         char, length = self.decoder.decode_next(text[last_input:])
         if char is None:
             return None
 
         self.document().clearUndoRedoStacks()
-        self.prev_input_indexes.append(last_input + length)
+        self._prev_input_indexes.append(last_input + length)
 
         return char
 
     def prev(self):
-        self.prev_input_indexes.pop()
+        self._prev_input_indexes.pop()
 
     def set_decoder(self, decoder: Type[BaseDecoder]):
         self.decoder = decoder
@@ -55,7 +55,7 @@ class StandardInputText(QtWidgets.QPlainTextEdit):
 
     def _can_add_text(self):
         """Return whether it would be valid for text to be inserted at the current cursor position."""
-        return self.textCursor().selectionStart() >= self.prev_input_indexes[-1]
+        return self.textCursor().selectionStart() >= self._prev_input_indexes[-1]
 
     def keyPressEvent(self, event):
         key = event.key()
@@ -70,7 +70,7 @@ class StandardInputText(QtWidgets.QPlainTextEdit):
             if (
                 textcursor.hasSelection()
                 and self._can_add_text()
-                or textcursor.position() - 1 >= self.prev_input_indexes[-1]
+                or textcursor.position() - 1 >= self._prev_input_indexes[-1]
             ):
                 textcursor.deletePreviousChar()
         elif key == QtCore.Qt.Key_Delete:
@@ -81,41 +81,75 @@ class StandardInputText(QtWidgets.QPlainTextEdit):
 
 
 class HighlightInputText(StandardInputText):
+
+    # An update request is emitted by next or prev, in case the
+    # input text is used in a different thread.
+    _update_request = QtCore.pyqtSignal()
+
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self.highlight_char_format = QtGui.QTextCharFormat()
         self.highlight_char_format.setBackground(QtGui.QColor(QtCore.Qt.gray))
 
+        self._current_highlight = (0, 0)
+
+        self._update_request.connect(self.highlight_current)
+
     def restart(self):
         self.remove_prev_highlight()
         self._reset()
 
-    def next_(self):
-        self.remove_prev_highlight()
+    def next_(self, update=0):
+        """
+        Return the next input character(s).
+
+        `update`:
+            - 0: don't update visual
+            - 1: update visual (using same thread)
+            - 2: update visual from main thread using
+                a signal (Call this if you are using the input
+                text in a different thread)
+        """
         char = super().next_()
-        self.highlight_current()
+
+        if update == 1:
+            self.highlight_current()
+        elif update == 2:
+            self._update_request.emit()
+
         return char
 
-    def prev(self):
-        self.remove_prev_highlight()
+    def prev(self, update=0):
+        """
+        `update`:
+            - 0: don't update visual
+            - 1: update visual (using same thread)
+            - 2: update visual from main thread using
+                a signal (Call this if you are using the input
+                text in a different thread)
+        """
         super().prev()
-        self.highlight_current()
+
+        if update == 1:
+            self.highlight_current()
+        elif update == 2:
+            self._update_request.emit()
 
     def highlight_current(self):
-        start = self.prev_input_indexes[-2]
-        end = self.prev_input_indexes[-1]
+        self.remove_prev_highlight()
 
-        self.format_range(start, end, self.highlight_char_format)
+        self._current_highlight = tuple(self._prev_input_indexes[-2:])
+        self.format_range(*self._current_highlight, self.highlight_char_format)
 
     def remove_prev_highlight(self):
-        start = self.prev_input_indexes[-2]
-        end = self.prev_input_indexes[-1]
-
-        self.format_range(start, end, self.default_char_format)
+        self.format_range(*self._current_highlight, self.default_char_format)
 
     def format_range(self, start, end, format_):
         textcursor = self.textCursor()
         textcursor.setPosition(start)
         textcursor.setPosition(end, QtGui.QTextCursor.KeepAnchor)
         textcursor.setCharFormat(format_)
+
+    def request_highlight_update(self):
+        self._update_request.emit()
